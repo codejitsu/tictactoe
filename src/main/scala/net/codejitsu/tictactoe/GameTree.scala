c932a1486dd96d6c218ce794a3320d651ec0318d
Alex <me@codejitsu.net>
2013-07-23 22:16:27 +0200

    Verified first two levels of god game tree.

--- src/main/scala/net/codejitsu/tictactoe/GameTree.scala
+++ src/main/scala/net/codejitsu/tictactoe/GameTree.scala
@@ -1,21 +1,30 @@
 package net.codejitsu.tictactoe
 
 import net.codejitsu.tictactoe.PlayerType._
+import scala.collection.immutable.HashMap
+import scala.collection.immutable.HashSet
 
 sealed trait GameTree[+T] {
   def nextPlayer: PlayerType
   def toList: List[T]
+  def mkString: String
 }
 
 case object Leaf extends GameTree[Nothing] {
   def nextPlayer = null
   def toList = Nil
+  def mkString = "-"
 }
 
 case class Node[T](e: Option[T], children: List[GameTree[T]], nextPlayer: PlayerType) extends GameTree[T] {
   def toList = e match {
     case Some(s) => s :: children.foldLeft(List[T]())(_ ::: _.toList)
     case _ => children.foldLeft(List[T]())(_ ::: _.toList)
+  }
+
+  def mkString = e match {
+    case Some(s) => "===" + "\n" + s.toString + "\n" + children.map(_.mkString).foldLeft("")(_ + _)
+    case _ => "===" + "\n" + "[]" + "\n" + children.map(_.mkString).foldLeft("")(_ + _)
   }
 }
 
@@ -24,42 +33,26 @@ object GameTree {
     Node[Field](Option(Field()), List[GameTree[Field]](), PlayerType.X)
   }
 
-  def build(tree: GameTree[Field], level: Int): GameTree[Field] = {
-    if (level == 0) Leaf
+  def build(tree: GameTree[Field], level: Int, upToLevel: Int): GameTree[Field] = {
+    if (upToLevel == 0) Leaf
+    else if (level == upToLevel) tree
     else {
       tree match {
         case Node(e, ch, p) => {
-          if (level == FieldSize * FieldSize) tree
+          if (upToLevel == FieldSize * FieldSize) tree
           else {
-            val totalMoves = FieldSize * FieldSize - level
+            val totalMoves = FieldSize * FieldSize - upToLevel
 
             val parentField = e.getOrElse(null)
 
             val player = Player("Player", p, new RandomMoveStrategy())
 
-            val map = scala.collection.mutable.HashMap.empty[String, Boolean]
-            val buf = scala.collection.mutable.ListBuffer.empty[Field]
-            
-            for (step <- 1 to totalMoves) {
-              for (i <- 0 until FieldSize) {
-                for (j <- 0 until FieldSize) {
-                  try {
-                    val nextField = parentField.update(Move(i, j, player))
-                    
-                    if (!map.contains(nextField.toString)) {
-                      map += (nextField.toString -> true)
-                      buf += nextField
-                    }
-                  } catch {
-                    case ise: IllegalStateException => ()
-                  }
-                }
-              }
-            }
+            val fields = collectFields(parentField, player, HashSet.empty[String], List.empty[Field], totalMoves,
+              moves)
 
-            val children = buf.toList.map(f => Node[Field](Option(f), List[GameTree[Field]](), nextPlayer(p)))
-            
-            Node[Field](e, children.map(build(_, level - 1)), nextPlayer(p))
+            val children = fields.map(f => Node[Field](Option(f), List[GameTree[Field]](), nextPlayer(p)))
+
+            Node[Field](e, children.map(build(_, level + 1, upToLevel)), nextPlayer(p))
           }
         }
 
@@ -68,8 +61,50 @@ object GameTree {
     }
   }
 
+  def moves = List((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2))
+
+  def collectFields(parentField: Field, player: Player,
+    hash: HashSet[String], acc: List[Field], total: Int, moves: List[(Int, Int)]): List[Field] = moves match {
+    case Nil => acc
+    case x :: tail =>
+      try {
+        val nextField = parentField.update(Move(x._1, x._2, player))
+
+        if (!hash.contains(nextField.toString)) {
+          collectFields(parentField, player, hash + nextField.toString, nextField :: acc, total - 1, tail)
+        } else {
+          collectFields(parentField, player, hash, acc, total - 1, tail)
+        }
+      } catch {
+        case ise: IllegalStateException => collectFields(parentField, player, hash, acc, total - 1, tail)
+      }
+  }
+
   def nextPlayer(player: PlayerType) = {
     if (player == PlayerType.X) PlayerType.O
     else PlayerType.X
+  }
+
+  def print(tree: GameTree[Field], startLevel: Int): Unit = tree match {
+    case Leaf => println("[]")
+    case Node(e, ch, p) => {
+      println("Level: " + startLevel)
+      val field = e.getOrElse(Field())
+      println(field.toString)
+
+      ch.map(c => print(c, startLevel + 1))
+    }
+  }
+
+  def printLevel(tree: GameTree[Field], level: Int, targetLevel: Int): Unit = tree match {
+    case Leaf => println("[]")
+    case Node(e, ch, p) => {
+      if (level == targetLevel - 1) {
+        println("Level: " + targetLevel)
+        ch.map(println(_))
+      } else {
+        ch.map(c => printLevel(c, level + 1, targetLevel))
+      }
+    }
   }
 }
--- src/test/scala/net/codejitsu/tictactoe/test/GameTreeTest.scala
+++ src/test/scala/net/codejitsu/tictactoe/test/GameTreeTest.scala
@@ -45,7 +45,7 @@ class GameTreeTest {
 	
 	@Test
 	def firstLevelIsNotEmptyConsistsOfNineElements() {
-	  val firstLevel = GameTree.build(GameTree.start, 1)
+	  val firstLevel = GameTree.build(GameTree.start, 1, 2)
 	  
 	  firstLevel match {
 	    case Leaf => fail()
@@ -54,5 +54,73 @@ class GameTreeTest {
 	      assertEquals(FieldSize * FieldSize, ch.size)
 	    }
 	  }
+	}
+	
+	@Test
+	def firstLevelAllElementsAreNodes() {
+	  val firstLevel = GameTree.build(GameTree.start, 1, 2)
+	  
+	  firstLevel match {
+	    case Leaf => fail()
+	    case Node(e, ch, p) => {
+	    	ch.foreach(c => c match {
+	    	  case Leaf => fail()
+	    	  case _ => ()
+	    	})
+	    }
+	  }
 	}	
+	
+	@Test
+	def firstLevelContainsAllCorrectMoves() {
+	  val firstLevel = GameTree.build(GameTree.start, 1, 2)
+	  
+	  val fieldInit = Field()
+	  val player = Player("Player", PlayerType.X, new RandomMoveStrategy)
+	  val moves =  List((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2))
+	  
+	  val allFields = moves.map(m => fieldInit.update(Move(m._1, m._2, player))).map(_.toString)
+	  
+	  firstLevel match {
+	    case Leaf => fail()
+	    case Node(e, ch, p) => {
+	    	ch.foreach(c => c match {
+	    	  case Leaf => fail()
+	    	  case x => allFields.contains(x.toString)
+	    	})
+	    }
+	  }	  
+	}
+	
+	@Test
+	def eachNodeOnSecondLevelHasEightChildren() {
+	  val firstLevel = GameTree.build(GameTree.start, 1, 2)
+	  val secondLevel = GameTree.build(firstLevel, 1, 3)
+
+	  secondLevel match {
+	    case Leaf => fail()
+	    case Node(e, ch, p) => {
+	    	ch.foreach(c => c match {
+	    	  case Leaf => fail()
+	    	  case Node(e2, ch2, p2) => {
+	    		  assertTrue(!ch2.isEmpty)
+	    		  assertEquals(FieldSize * FieldSize - 1, ch2.size)
+	    	  }
+	    	})	      
+	    }
+	  }
+	}
+	
+	@Test
+	def generateCompleteTree() {
+	  val tree = buildTree(GameTree.start, 1, 9)
+	 // GameTree.printLevel(tree, 0, 2)
+	}
+	
+	def buildTree(tree: GameTree[Field], level: Int, upToLevel: Int): GameTree[Field] = {
+	  if (level == upToLevel) tree
+	  else {
+	    buildTree(GameTree.build(tree, 1, level + 1), level + 1, upToLevel)
+	  }
+	}
 }
